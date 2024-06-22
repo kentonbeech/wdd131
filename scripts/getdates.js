015e10ee62d8f90ad04abb442dc42f38c5f178f8
Kenton Beech <bee23002@byui.edu>
2024-06-22 15:08:17 +0200

    Completed week 01 assignment.

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+const year = document.querySelector('#currentyear');
+
+const today = new Date();
+
+year.innerHTML =
+	currentyear.innerHTML = `@<span class="highlight">${today.getFullYear()}</span>`;
+
+const lastModified = document.lastModified;
+
+document.getElementById(
+	'lastModified'
+).textContent = `Last Modification: ${lastModified}`;
